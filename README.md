9e1857ae3b9b962609788b6700bb15cbc701fb63
harivardan-m <harivardan4466@gmail.com>
2024-02-01 15:16:57 -0500

    General edits and reset the code to the default code

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 This repo is used as a test repo to see how github handles white space changes.
 
 Please run the setup-hooks to set up the hook.
+Look into the commits to observe the changes
--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 print("hello world")
 
-#This is a comment with one space initially between >  <.
+#This is a comment with one space initially between > <.
 for i in range(2):
     print("In loop",i)
 
-#line 8 doesnt contain a trailing whitespace
-a=10 
+#line 8 (a= 10) doesnt contain a trailing whitespace initially
+a=10
 
 if a<20:
     print("a <20")
     print("a could be negative")
-    print ("a could be 0") #This line can be moved iniside the loop to check for leading white spaces
+print ("a could be 0") #This line can be moved inside the 'if' condition to check for leading white spaces 
+# End of code. A new line feed can be entered after this line
