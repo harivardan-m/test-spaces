85e9d05c2c5b1443a3485d2720ddf987d18cdf9b
harivardan-m <harivardan4466@gmail.com>
2024-02-01 14:13:50 -0500

    Added spaces to python code

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 print("hello world")
 
-#This is a comment with one space initially between > <.
+#This is a comment with one space initially between >  <.
 for i in range(2):
     print("In loop",i)
 
 #line 8 doesnt contain a trailing whitespace
-a=10
+a=10 
 
 if a<20:
     print("a <20")
     print("a could be negative")
-print ("a could be 0") #This line can be moved iniside the loop to check for leading white spaces
+    print ("a could be 0") #This line can be moved iniside the loop to check for leading white spaces
