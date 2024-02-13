18a7c9432a501ad7b5f200e128e019c500e1e954
harivardan-m <harivardan4466@gmail.com>
2024-02-12 19:45:49 -0500

    Edited whitespace to fix errors

--- main.py
+++ main.py
@@ -13,4 +13,3 @@ if a<20:
     print ("a could be 0") #This line can be moved inside the 'if' condition to check for leading white spaces 
 # End of code. A new line feed can be entered after this line
     
-    
